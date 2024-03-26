106844fe4cfdec9a0590c0aa7bf3d0b0ddc3d650
Arsen Chernov <arsenchernov02@gmail.com>
2024-03-26 16:25:39 +0600

    Added cors, modified message list

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import messagesRouter from "./routers/messages";
 import fileDb from "./fileDb";
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/messages', messagesRouter);
 
